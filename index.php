cc1ed324e43442dcae681011c08b4847c3fcb83d
Michiel van Eerd <michiel.van.eerd@peercode.nl>
2022-02-09 16:28:50 +0100

    Divider

--- index.php
+++ index.php
@@ -13,14 +13,25 @@ if (have_posts()) {
             } else {
                 the_title('<h1><a href="' . esc_url(get_permalink()) . '">', '</a></h1>');
             }
+            if (!is_page()) {
             ?>
             <div class="meta">
                 <div><time datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date(); ?></time></div>
                 <div><?php the_tags(); ?></div>
             </div>
+            <?php
+            }
+            ?>
         <div class="my-post-content">
         <?php
-        is_singular() ? the_content() : the_excerpt();
+        if (is_singular()) {
+            the_content();
+         } else {
+             the_excerpt();
+             if (($wp_query->current_post + 1) !== $wp_query->post_count) {
+                echo '<div class="mve-post-divider">∽</div>';
+             }
+         }
         ?></div></article><?php
     }
 } else {
@@ -30,10 +41,10 @@ if (have_posts()) {
 if (!is_singular()) {
 	$html = get_the_posts_pagination();
 	if (!empty($html)) {
-		echo '<hr>' . $html;
+		echo '<hr class="wp-block-separator">' . $html;
 	}
 }
 
-echo '<hr>';
+echo '<hr class="wp-block-separator">';
 
 get_footer();
